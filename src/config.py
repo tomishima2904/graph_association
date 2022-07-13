6b033c69ede2622b5ece72e33648e4364db24293
tomishima <pumoco2904@icloud.com>
2022-07-13 09:19:43 +0900

    fit2022_v2→fit2022v2

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ parser = argparse.ArgumentParser("Setting configurations")
 
 # if type=strtobool, True -> 't' or 'True', False -> 'f' or 'False'
 parser.add_argument('--comparator', default='dot', type=str, help="[dot, cos, l2, squared_l2]")  # Only 'dot' is here!
-parser.add_argument('--dataset_path', default='fit2022_v2.csv', type=str, help="csvfile name of dataset")
+parser.add_argument('--dataset_path', default='fit2022v2.csv', type=str, help="csvfile name of dataset")
 parser.add_argument('--emb_model', default='embeddings_all_0_v50.pickle', type=str, help="picklefile name of trained embedding model")
 parser.add_argument('--get_date', default=None, help='date_time for path name')
 parser.add_argument('--pages_tsv', default='jawiki-20220601-page.sql.tsv', type=str, help="tsvfile name of wikidata page")
